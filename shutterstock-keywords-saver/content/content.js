1a64f47d0dc53f21167afa59df0fa7216ad5fcd1
Slavik <vshnitser@globaltechincubator.com.au>
2021-03-02 21:35:15 +0200

    Fix issue with DOM manipulations

--- shutterstock-keywords-saver/content/content.js
+++ shutterstock-keywords-saver/content/content.js
@@ -1,6 +1,12 @@
 (() => {
   /** CSS selector for keywords container. */
-  const containerSelector = '.C_a_c';
+  const containerSelector = '[data-automation=ExpandableKeywordsList_container_div]';
+  /**
+   * The prefix of all CSS classes used by extension. This approach helps to avoid collisions
+   * between custom class names and web-page class name.
+   * @type {string}
+   */
+  const CSS_PREFIX = "shutterstock-extension";
 
   /**
    * Creates button to copy all keywords.
@@ -9,11 +15,10 @@
    */
   function addCopyBtn(container, keywords) {
     const containerParent = container.parentElement;
-    const classes = containerParent.querySelector('button').classList;
     const copyButton = document.createElement('button');
 
     copyButton.innerText = 'Copy all';
-    copyButton.classList = classes;
+    copyButton.classList.add(`${CSS_PREFIX}__copy-btn`);
     copyButton.onclick = () => {
       chrome.runtime.sendMessage({type: "addKeywords", keywords});
     };
